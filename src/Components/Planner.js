d527622e00de06525bca606a5e77203656608a43
River <swimmingr@gmail.com>
2023-03-12 21:44:30 +0900

    add planner

--- src/Components/Planner.js
+++ src/Components/Planner.js
@@ -1,5 +1,64 @@
-function Planner() {
-    return <>planner</>;
+import Calendar from "react-calendar";
+import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+import styled from "styled-components";
+
+const StyledCalendar = styled(Calendar)`
+  & .react-calendar {
+    border: 1px solid #ccc;
+    font-family: Arial, sans-serif;
+    border-radius: 5px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
   }
-  export default Planner;
-  
+
+  & .react-calendar__month-view__days__day {
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  & .react-calendar__tile--now {
+    background-color: #b3d9ff;
+  }
+`;
+
+function Planner() {
+  const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+  const [selectedDates, setSelectedDates] = useState([]);
+
+  const handleDateChange = (date) => {
+    // 선택한 기간의 모든 날짜 구하기
+    const dates = [];
+    let currentDate = new Date(date[0]);
+
+    while (currentDate <= date[date.length - 1]) {
+      dates.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    setSelectedDates(dates);
+    setDateRange(date);
+  };
+  return (
+    <>
+      planner
+      <h2>where?</h2>
+      <StyledCalendar
+        value={dateRange}
+        onChange={handleDateChange}
+        selectRange={true}
+      />
+      {selectedDates.length === 0 ? (
+        <h2>시작/끝 날짜를 선택하세요</h2>
+      ) : (
+        <button>
+          {selectedDates[0]?.getFullYear()}.{selectedDates[0]?.getMonth()}.
+          {selectedDates[0]?.getDate()}~
+          {selectedDates[selectedDates.length - 1]?.getFullYear()}.
+          {selectedDates[selectedDates.length - 1]?.getMonth()}.
+          {selectedDates[selectedDates.length - 1]?.getDate()}
+        </button>
+      )}
+    </>
+  );
+}
+export default Planner;
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,13 +1,33 @@
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  border: solid 1px black;
+  width: 80vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+const Box = styled.div``;
+const ImageBox = styled.div`
+  width: 5em;
+  height: 5em;
+  border: solid 1px black;
+  border-radius: 50px;
+  overflow: hidden;
+`;
 function Profile() {
   const me = useSelector((state) => state.filter((v) => v.on === true));
   return (
-    <>
-    <h1>profile</h1>
-      <h1>{`${me[0].nickname} profile`}</h1>
-      <h2>{`${me[0].nickname} info`}</h2>
-    </>
+    <Wrapper>
+      <ImageBox>{/* <img src={require("")} /> */}</ImageBox>
+      <p>profie edit</p>
+      <h1>{`${me[0].nickname}`}</h1>
+      <h2>내 리뷰 </h2>
+      <h2>내 여행</h2>
+    </Wrapper>
   );
 }
 export default Profile;
