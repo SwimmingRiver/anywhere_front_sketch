f24b9a06b87002a1d589e583ab5cca796c76b75c
River <swimmingr@gmail.com>
2023-04-08 22:11:57 +0900

    add routers

---
+++ src/Components/Routers.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+
+import Join from "../Components/Join";
+import Home from "../Pages/Home";
+import Login from "../Components/Login";
+import Kakao from "../Util/Kakao";
+import Search from "../Components/Search";
+import LodgeInfo from "../Components/LodgeInfo";
+import Planner from "../Components/Planner";
+import Profile from "../Pages/Profile";
+import Place from "../Components/Place";
+import EditProfile from "./../Components/EditProfile";
+import MyPlan from "../Components/MyPlan";
+import Payment from "../Components/Payment";
+
+function Routers() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/kakaologin" element={<Kakao />} />
+      <Route path="/lodgeinfo/:id/:room" element={<LodgeInfo />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/planner" element={<Planner />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/place/:id" element={<Place />} />
+      <Route path="/editprofile" element={<EditProfile />} />
+      <Route path="/myplan" element={<MyPlan />} />
+      <Route path="/payment" element={<Payment />} />
+    </Routes>
+  );
+}
+export default Routers;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,19 +1,8 @@
-import Join from "../Components/Join";
 import styled from "styled-components";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Login from "../Components/Login";
-import Nav from "../Nav";
-import Kakao from "../Util/Kakao";
-import Search from "../Components/Search";
-import LodgeInfo from "../Components/LodgeInfo";
-import Planner from "../Components/Planner";
 import { useSelector } from "react-redux";
-import Profile from "./Profile";
-import Place from "../Components/Place";
-import EditProfile from "./../Components/EditProfile";
-import MyPlan from "../Components/MyPlan";
-import Payment from "../Components/Payment";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Routers from "../Components/Routers";
+
 
 const Title = styled.h1`
   font-size: 1em;
@@ -78,21 +67,7 @@ function Main() {
               </Link>
             </Title>
           </HeadWrapper>
-
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/kakaologin" element={<Kakao />} />
-            <Route path="/lodgeinfo/:id/:room" element={<LodgeInfo />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/planner" element={<Planner />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/place/:id" element={<Place />} />
-            <Route path="/editprofile" element={<EditProfile />} />
-            <Route path="/myplan" element={<MyPlan />} />
-            <Route path="/payment" element={<Payment />} />
-          </Routes>
+          <Routers/>
           <Lower>
             <Item>
               <Link to="/">
